c9665ba35244748a649c58659b22bf5d4ea3b29a
lokeshreddy07 <lokesh0709@gmail.com>
2018-07-05 13:54:00 +0530

    added sop stmt

--- src/com/hp/test/HelloWorld.java
+++ src/com/hp/test/HelloWorld.java
@@ -3,6 +3,7 @@ package com.hp.test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		System.out.println("for loop starts");
 		for(int i=0;i<=10;i++){
 		System.out.println("Welcome to Git world!!"+i);
 		}
