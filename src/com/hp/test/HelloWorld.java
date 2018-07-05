375290dac3c841cd26c62732f066cb8d73be0ace
lokeshreddy07 <lokesh0709@gmail.com>
2018-07-05 12:58:47 +0530

    add loop

--- src/com/hp/test/HelloWorld.java
+++ src/com/hp/test/HelloWorld.java
@@ -3,9 +3,9 @@ package com.hp.test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Welcome to Git world!!");
-
+		for(int i=0;i<=10;i++){
+		System.out.println("Welcome to Git world!!"+i);
+		}
 	}
 
 }
