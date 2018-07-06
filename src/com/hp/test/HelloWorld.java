a96307420e85a890586c0422a1fd9d357b07ce85
lokeshreddy07 <lokesh0709@gmail.com>
2018-07-06 12:16:21 +0530

    updated sop

--- src/com/hp/test/HelloWorld.java
+++ src/com/hp/test/HelloWorld.java
@@ -3,7 +3,7 @@ package com.hp.test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("for loop starts");
+		System.out.println("for loop starts and will run");
 		for(int i=0;i<=10;i++){
 		System.out.println("Welcome to Git world!!"+i);
 		}
